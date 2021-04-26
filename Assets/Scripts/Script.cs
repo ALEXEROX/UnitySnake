033cdf6f666238fb51a0160a2d5c37ba2a0e6ade
alexerox <ermakov.alexerox@yandex.ru>
2021-04-26 19:18:06 +0300

    Kvantorium 26.04.21 Remove other problem

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -6,6 +6,7 @@ using System.Timers;
 public class Script : MonoBehaviour
 {
     static Pos right = new Pos(1, 0), left = new Pos(-1, 0), up = new Pos(0, 1), down = new Pos(0, -1);
+    Snake snake;
     class Snake
     {
         public GameObject[] body;
@@ -32,31 +33,30 @@ public class Script : MonoBehaviour
     }
     void Start()
     {
-        InvokeRepeating("Cadr", 0f, 0.25f);
         GameObject Head = GameObject.Find("Head"), Body = GameObject.Find("Body"), Bone = GameObject.Find("Bone"), Apple = GameObject.Find("Apple");
+        GameObject[] alfa = { Instantiate(Head), Instantiate(Body), Instantiate(Body), Instantiate(Body) }, beta = { Instantiate(Bone), Instantiate(Bone), Instantiate(Bone) };//Add start position
+        Pos[] gamma = { right, right, right };
+        Pos delta = right;
+        snake = new Snake(alfa, beta, gamma, delta);
+        InvokeRepeating("Cadr", 0f, 0.250f);
     }
     void FixedUpdate()
     {
-        //if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-        //    snake.futuredirection = up;
-        //if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-        //    snake.futuredirection = down;
-        //if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-        //    snake.futuredirection = right;
-        //if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-        //    snake.futuredirection = left;
-    }
-    void moveSnake()
-    {
-
-    }
-    void drawSnake()
-    {
-
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            snake.futuredirection = up;
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            snake.futuredirection = down;
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            snake.futuredirection = right;
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            snake.futuredirection = left;
     }
     void Cadr()
     {
-        moveSnake();
-        drawSnake();
+        for(int i = snake.body.Length - 1; i > 0; i--)
+            snake.body[i].transform.position = snake.body[i - 1].transform.position;
+        snake.body[0].transform.position = new Vector3(snake.body[0].transform.position.x + snake.futuredirection.x, snake.body[0].transform.position.y + snake.futuredirection.y, 0f);
+        for (int i = snake.direction.Length - 1; i >= 0; i--)
+            snake.spine[i].transform.position = new Vector3(snake.body[i].transform.position.x + snake.direction[i].x, snake.body[i].transform.position.y + snake.direction[i].y, 0f);
     }
 }
